3809d309f843937e7f34e6ec80384471cc57a4c0
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-01-09 22:07:34 +0530

    identicon added.

--- app/src/app/app.service.ts
+++ app/src/app/app.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
 import { ethers } from "ethers";
+import Identicon from "identicon.js";
 declare const window: any;
 
 @Injectable()
 export class AppService {
   accounts: string[] = [];
   balance: string;
+  imageData;
   constructor() {
   }
   async getWalletDetails() {
@@ -14,11 +16,9 @@ export class AppService {
     if (this.accounts.length > 0) {
       let balanceObj = await provider.getBalance(this.accounts[0]);
       this.balance = ethers.utils.formatEther(balanceObj);
+      this.imageData = new Identicon(this.accounts[0], 30).toString();
+      this.imageData = "data:image/png;base64," + this.imageData;
     }
-
     console.log(provider);
-    console.log(this.accounts);
-    console.log(this.balance);
-
   }
 }
